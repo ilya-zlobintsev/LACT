d3f66f4e6186fe5390b90a0dd878c4a417b1299e
Ilya Zlobintsev <ilya.zl@protonmail.com>
2021-02-28 07:50:57 +0200

    Show fan curve in CLI

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,25 +1,38 @@
+use colored::*;
 use daemon::daemon_connection::DaemonConnection;
 use structopt::StructOpt;
-use colored::*;
 
 #[derive(StructOpt)]
-#[structopt(rename_all = "lower")]
-enum Opt {
-    ///Realtime GPU information
-    Stats {
-        gpu_id: Option<u32>,
-    },
-    ///Get GPU list
-    Gpus,
-    ///General information about the GPU
-    Info {
+enum Curve {
+    /// Shows current fan control information
+    Status {
+        /// Specify a GPU ID as printed in `lact-cli gpus`. By default, all GPUs are printed.
         gpu_id: Option<u32>,
     },
 }
 
+#[derive(StructOpt)]
+#[structopt(rename_all = "lower")]
+enum Opt {
+    /// Realtime GPU information
+    Metrics {
+        /// Specify a GPU ID as printed in `lact-cli gpus`. By default, all GPUs are printed.
+        gpu_id: Option<u32>,
+    },
+    /// Get GPU list
+    Gpus,
+    /// General information about the GPU
+    Info {
+        /// Specify a GPU ID as printed in `lact-cli gpus`. By default, all GPUs are printed.
+        gpu_id: Option<u32>,
+    },
+    /// Fan curve control
+    Curve(Curve),
+}
+
 fn main() {
     env_logger::init();
-    
+
     let opt = Opt::from_args();
 
     let d = DaemonConnection::new().unwrap();
@@ -29,14 +42,13 @@ fn main() {
         Opt::Gpus => {
             let gpus = d.get_gpus();
             println!("{:?}", gpus);
-        },
-        Opt::Stats { gpu_id } => {
+        }
+        Opt::Metrics { gpu_id } => {
             let mut gpu_ids: Vec<u32> = Vec::new();
 
             if let Some(gpu_id) = gpu_id {
                 gpu_ids.push(gpu_id);
-            }
-            else {
+            } else {
                 for (gpu_id, _) in d.get_gpus().unwrap() {
                     gpu_ids.push(gpu_id);
                 }
@@ -45,14 +57,13 @@ fn main() {
             for gpu_id in gpu_ids {
                 print_stats(&d, gpu_id);
             }
-        },
+        }
         Opt::Info { gpu_id } => {
             let mut gpu_ids: Vec<u32> = Vec::new();
 
             if let Some(gpu_id) = gpu_id {
                 gpu_ids.push(gpu_id);
-            }
-            else {
+            } else {
                 for (gpu_id, _) in d.get_gpus().unwrap() {
                     gpu_ids.push(gpu_id);
                 }
@@ -61,27 +72,124 @@ fn main() {
             for gpu_id in gpu_ids {
                 print_info(&d, gpu_id);
             }
+        }
+        Opt::Curve(curve) => match curve {
+            Curve::Status { gpu_id } => {
+                let mut gpu_ids: Vec<u32> = Vec::new();
+
+                if let Some(gpu_id) = gpu_id {
+                    gpu_ids.push(gpu_id);
+                } else {
+                    for (gpu_id, _) in d.get_gpus().unwrap() {
+                        gpu_ids.push(gpu_id);
+                    }
+                }
+
+                for gpu_id in gpu_ids {
+                    print_fan_curve(&d, gpu_id);
+                }
+            }
         },
+    }
+}
+
+fn print_fan_curve(d: &DaemonConnection, gpu_id: u32) {
+    let fan_control = d.get_fan_control(gpu_id).unwrap();
+
+    if fan_control.enabled {
+        println!("{}", "Fan curve:".yellow());
+
+        for (temp, fan_speed) in fan_control.curve {
+            println!(
+                "{}{}: {}{}",
+                temp.to_string().yellow(),
+                "C°".yellow(),
+                fan_speed.round().to_string().bold(),
+                "%".bold()
+            );
+        }
+    } else {
+        println!("{}", "Automatic fan control used".yellow());
     }
 }
 
 fn print_info(d: &DaemonConnection, gpu_id: u32) {
     let gpu_info = d.get_gpu_info(gpu_id).unwrap();
-    println!("{} {}", "GPU Model:".blue(), gpu_info.vendor_data.card_model.unwrap_or_default().bold());
-    println!("{} {}", "GPU Vendor:".blue(), gpu_info.vendor_data.gpu_vendor.unwrap_or_default().bold());
+    println!(
+        "{} {}",
+        "GPU Model:".blue(),
+        gpu_info.vendor_data.card_model.unwrap_or_default().bold()
+    );
+    println!(
+        "{} {}",
+        "GPU Vendor:".blue(),
+        gpu_info.vendor_data.gpu_vendor.unwrap_or_default().bold()
+    );
     println!("{} {}", "Driver in use:".blue(), gpu_info.driver.bold());
-    println!("{} {}", "VBIOS Version:".blue(), gpu_info.vbios_version.bold());
-    println!("{} {}", "VRAM Size:".blue(), gpu_info.vram_size.to_string().bold());
+    println!(
+        "{} {}",
+        "VBIOS Version:".blue(),
+        gpu_info.vbios_version.bold()
+    );
+    println!(
+        "{} {}",
+        "VRAM Size:".blue(),
+        gpu_info.vram_size.to_string().bold()
+    );
     println!("{} {}", "Link Speed:".blue(), gpu_info.link_speed.bold());
 }
 
 fn print_stats(d: &DaemonConnection, gpu_id: u32) {
     let gpu_stats = d.get_gpu_stats(gpu_id).unwrap();
-    println!("{} {}/{} MiB", "VRAM Usage:".green(), gpu_stats.mem_used.unwrap_or_default(), gpu_stats.mem_total.unwrap_or_default());
-    println!("{} {}°C", "Temperature:".green(), gpu_stats.gpu_temp.unwrap_or_default());
-    println!("{} {}/{}RPM", "Fan Speed:".green(), gpu_stats.fan_speed.unwrap_or_default(), gpu_stats.max_fan_speed.unwrap_or_default());
-    println!("{} {}MHz", "GPU Clock:".green(), gpu_stats.gpu_freq.unwrap_or_default());
-    println!("{} {}V", "GPU Voltage:".green(), gpu_stats.voltage.unwrap_or_default() as f64 / 1000.0);
-    println!("{} {}MHz", "VRAM Clock:".green(), gpu_stats.mem_freq.unwrap_or_default());
-    println!("{} {}/{}W", "Power Usage:".green(), gpu_stats.power_avg.unwrap_or_default(), gpu_stats.power_cap.unwrap_or_default());
+    println!(
+        "{} {}/{}{}",
+        "VRAM Usage:".green(),
+        gpu_stats.mem_used.unwrap_or_default().to_string().bold(),
+        gpu_stats.mem_total.unwrap_or_default().to_string().bold(),
+        "MiB".bold(),
+    );
+    println!(
+        "{} {}{}",
+        "Temperature:".green(),
+        gpu_stats.gpu_temp.unwrap_or_default().to_string().bold(),
+        "°C".bold(),
+    );
+    println!(
+        "{} {}/{}{}",
+        "Fan Speed:".green(),
+        gpu_stats.fan_speed.unwrap_or_default().to_string().bold(),
+        gpu_stats
+            .max_fan_speed
+            .unwrap_or_default()
+            .to_string()
+            .bold(),
+        "RPM".bold(),
+    );
+    println!(
+        "{} {}{}",
+        "GPU Clock:".green(),
+        gpu_stats.gpu_freq.unwrap_or_default().to_string().bold(),
+        "MHz".bold(),
+    );
+    println!(
+        "{} {}{}",
+        "GPU Voltage:".green(),
+        (gpu_stats.voltage.unwrap_or_default() as f64 / 1000.0)
+            .to_string()
+            .bold(),
+        "V".bold(),
+    );
+    println!(
+        "{} {}{}",
+        "VRAM Clock:".green(),
+        gpu_stats.mem_freq.unwrap_or_default().to_string().bold(),
+        "MHz".bold(),
+    );
+    println!(
+        "{} {}/{}{}",
+        "Power Usage:".green(),
+        gpu_stats.power_avg.unwrap_or_default().to_string().bold(),
+        gpu_stats.power_cap.unwrap_or_default().to_string().bold(),
+        "W".bold(),
+    );
 }
