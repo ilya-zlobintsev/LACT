bf80dcc34191df00d90e9954a6fd5e61c327fc0e
Ilya Zlobintsev <ilya.zl@protonmail.com>
2023-01-05 13:01:03 +0200

    feat: make gui optional

--- lact/src/main.rs
+++ lact/src/main.rs
@@ -7,6 +7,18 @@ fn main() -> anyhow::Result<()> {
     let command = Command::parse();
     match command {
         Command::Daemon => lact_daemon::run(),
-        Command::Gui => lact_gui::run(),
+        Command::Gui => run_gui(),
     }
+}
+
+#[cfg(feature = "lact-gui")]
+fn run_gui() -> anyhow::Result<()> {
+    lact_gui::run()
+}
+
+#[cfg(not(feature = "lact-gui"))]
+fn run_gui() -> anyhow::Result<()> {
+    use anyhow::anyhow;
+
+    Err(anyhow!("LACT was built without GUI support"))
 }
