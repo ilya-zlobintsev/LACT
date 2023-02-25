2067641feee50647c92f12985dd821cf220d54a0
Ilya Zlobintsev <ilya.zl@protonmail.com>
2023-02-25 19:08:58 +0200

    chore: remove old uninstall script

--- uninstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-sudo systemctl disable --now lactd &&
-sudo rm /usr/local/bin/lact-daemon &&
-sudo rm /usr/local/bin/lact-gui &&
-sudo rm /etc/systemd/system/lactd.service &&
-sudo rm /usr/local/share/applications/lact.desktop
