92eb1002392284d45a71a48087cf7a084dc3a656
Ilya Zlobintsev <ilya.zl@protonmail.com>
2023-05-14 13:09:15 +0300

    feat: bump version and report gui version

--- lact-gui/src/app/root_stack/software_page.rs
+++ lact-gui/src/app/root_stack/software_page.rs
@@ -29,14 +29,45 @@ pub fn software_page(system_info: SystemInfo, embedded: bool) -> Grid {
     if embedded {
         daemon_version.push_str("-embedded");
     }
-    let version_label = Label::builder()
+    let daemon_version_label = Label::builder()
         .use_markup(true)
         .label(format!("<b>{daemon_version}</b>"))
         .hexpand(true)
         .halign(Align::Start)
         .build();
 
-    container.attach(&version_label, 1, 0, 1, 1);
+    container.attach(&daemon_version_label, 1, 0, 1, 1);
+
+    container.attach(
+        &{
+            let label = Label::new(None);
+            label.set_markup("LACT GUI:");
+            label.set_halign(Align::End);
+            label.set_hexpand(true);
+            label
+        },
+        0,
+        1,
+        1,
+        1,
+    );
+
+    let gui_version = env!("CARGO_PKG_VERSION");
+    let gui_profile = if cfg!(debug_assertions) {
+        "debug"
+    } else {
+        "release"
+    };
+    let gui_version = format!("{gui_version}-{gui_profile}");
+
+    let gui_version_label = Label::builder()
+        .use_markup(true)
+        .label(format!("<b>{gui_version}</b>"))
+        .hexpand(true)
+        .halign(Align::Start)
+        .build();
+
+    container.attach(&gui_version_label, 1, 1, 1, 1);
 
     container.attach(
         &Label::builder()
@@ -45,7 +76,7 @@ pub fn software_page(system_info: SystemInfo, embedded: bool) -> Grid {
             .hexpand(true)
             .build(),
         0,
-        1,
+        2,
         1,
         1,
     );
@@ -55,7 +86,7 @@ pub fn software_page(system_info: SystemInfo, embedded: bool) -> Grid {
         .hexpand(true)
         .halign(Align::Start)
         .build();
-    container.attach(&kernel_version_label, 1, 1, 1, 1);
+    container.attach(&kernel_version_label, 1, 2, 1, 1);
 
     container
 }
