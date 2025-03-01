79d1ac9ede8b9f097c24f1bb985e7814c86ee05b
Ilya Zlobintsev <ilya.zl@protonmail.com>
2025-03-01 09:56:10 +0200

    feat: better amd fan sensor checks, retry if sensor becomes missing

--- lact-daemon/src/server/gpu_controller/amd.rs
+++ lact-daemon/src/server/gpu_controller/amd.rs
@@ -45,6 +45,7 @@ use {
 };
 
 const GPU_CLOCKDOWN_TIMEOUT_SECS: u64 = 3;
+const FAN_CONTROL_RETRIES: u32 = 10;
 const MAX_PSTATE_READ_ATTEMPTS: u32 = 5;
 const STEAM_DECK_IDS: [&str; 2] = ["163F", "1435"];
 
@@ -201,7 +202,19 @@ impl AmdGpuController {
             1 => {
                 warn!("GPU has only one temperature sensor, 'temperature_key' setting will be ignored");
             }
-            _ => (),
+            _ => {
+                if !temps.contains_key(&settings.temperature_key) {
+                    return Err(anyhow!(
+                        "Sensor with name {} not found, available sensors: {}",
+                        settings.temperature_key,
+                        temps
+                            .keys()
+                            .map(String::as_str)
+                            .collect::<Vec<&str>>()
+                            .join(",")
+                    ));
+                }
+            }
         }
 
         hw_mon
@@ -230,6 +243,8 @@ impl AmdGpuController {
             #[allow(clippy::cast_precision_loss)]
             let change_threshold = settings.change_threshold.unwrap_or(0) as f32;
 
+            let mut retries = 0;
+
             loop {
                 select! {
                     () = sleep(interval) => (),
@@ -239,11 +254,19 @@ impl AmdGpuController {
                 let mut temps = hw_mon.get_temps();
                 let temp = if temps.len() == 1 {
                     temps.into_values().next().unwrap()
+                } else if let Some(value) = temps.remove(&temp_key) {
+                    value
                 } else {
-                    temps
-                        .remove(&temp_key)
-                        .expect("Could not get temperature by given key")
+                    retries += 1;
+
+                    if retries == FAN_CONTROL_RETRIES {
+                        error!("could not get temperature sensor {temp_key}, exiting fan control (reached max attempts)");
+                        break;
+                    }
+                    error!("could not get temperature sensor {temp_key} from {} sensors (assuming error is temporary, attempt {retries}/{FAN_CONTROL_RETRIES})", temps.len());
+                    continue;
                 };
+                retries = 0;
 
                 let current_temp = temp.current.expect("Missing temp");
 
