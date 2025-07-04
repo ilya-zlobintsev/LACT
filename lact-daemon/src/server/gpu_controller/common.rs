0d65ddb8c0ce18721e8aad3ac656398e03ee08fd
Ilya Zlobintsev <ilya.zl@protonmail.com>
2025-07-04 21:02:12 +0300

    feat: use cmdline as fallback for process name when exe is not available

--- lact-daemon/src/server/gpu_controller/common.rs
+++ lact-daemon/src/server/gpu_controller/common.rs
@@ -3,11 +3,23 @@ pub mod fdinfo;
 
 use libcopes::PID;
 use std::io;
+use tracing::debug;
 
 pub fn resolve_process_name(pid: PID) -> io::Result<(String, String)> {
-    let exe = libcopes::io::exe_reader(pid)?;
     let cmdline = libcopes::io::cmdline_reader(pid)?;
-    let name = libcopes::get_process_executed_file(exe, &cmdline).to_string();
+
+    let name = match libcopes::io::exe_reader(pid) {
+        Ok(exe) => libcopes::get_process_executed_file(exe, &cmdline).to_string(),
+        Err(err) => {
+            debug!("could not fetch exe for {pid}: {err}");
+            let args = cmdline.as_ref();
+            args.first().and_then(|arg| arg.to_str()).map_or_else(
+                || "<Unknown>".to_owned(),
+                |arg| arg.split('/').last().unwrap_or(arg).to_owned(),
+            )
+        }
+    };
+
     let args = cmdline
         .as_ref()
         .iter()
