56d550fe667acb741f1eea3cfd2637b755079558
Roman Makarov <rmakarov94@gmail.com>
2026-02-26 21:59:11 +0100

    clean up

--- lact-schema/src/config.rs
+++ lact-schema/src/config.rs
@@ -49,7 +49,6 @@ pub struct GpuConfig {
     pub custom_power_profile_mode_hueristics: Vec<Vec<Option<i32>>>,
     #[serde(default, skip_serializing_if = "IndexMap::is_empty")]
     pub power_states: IndexMap<PowerLevelKind, Vec<u8>>,
-    /// Initial target temperature captured from hardware (Nvidia only, persisted for reset)
     #[serde(default, skip_serializing_if = "Option::is_none")]
     pub initial_target_temp: Option<u32>,
 }
