bf5d46902758612091f779cd2f9be94caecbb00e
Roman Makarov <rmakarov94@gmail.com>
2026-04-28 22:10:24 +0200

    wip

--- lact-gui/src/app/error_dialog.rs
+++ lact-gui/src/app/error_dialog.rs
@@ -36,27 +36,23 @@ impl relm4::Component for ErrorDialog {
             set_default_response: Some(RESPONSE_CLOSE),
             set_content_width: 700,
 
-            add_response: (RESPONSE_COPY_CLOSE, "Copy and close"),
             add_response: (RESPONSE_CLOSE, "Close"),
 
             #[wrap(Some)]
-            set_extra_child = &gtk::Expander {
-                set_label: Some("Details"),
+            set_extra_child = &gtk::ScrolledWindow {
+                set_visible: cfg!(debug_assertions),
+                set_min_content_width: 600,
+                set_min_content_height: 240,
+                set_max_content_height: 360,
+                set_hscrollbar_policy: gtk::PolicyType::Automatic,
+                set_vscrollbar_policy: gtk::PolicyType::Automatic,
 
-                gtk::ScrolledWindow {
-                    set_min_content_width: 600,
-                    set_min_content_height: 240,
-                    set_max_content_height: 360,
-                    set_hscrollbar_policy: gtk::PolicyType::Automatic,
-                    set_vscrollbar_policy: gtk::PolicyType::Automatic,
-
-                    gtk::TextView {
-                        set_buffer: Some(&model.details_buffer),
-                        set_editable: false,
-                        set_cursor_visible: true,
-                        set_monospace: true,
-                        set_wrap_mode: gtk::WrapMode::None,
-                    }
+                gtk::TextView {
+                    set_buffer: Some(&model.details_buffer),
+                    set_editable: false,
+                    set_cursor_visible: true,
+                    set_monospace: true,
+                    set_wrap_mode: gtk::WrapMode::None,
                 }
             }
         }
@@ -96,13 +92,12 @@ impl relm4::Component for ErrorDialog {
                 self.details = format!("{err:?}");
                 warn!("{}", self.details);
                 self.details_buffer.set_text(&self.details);
+                self.update_view(widgets, sender);
                 root.present(Some(&self.parent));
             }
             ErrorDialogMsg::CopyAndClose => {
                 root.clipboard().set_text(&self.details);
             }
         }
-
-        self.update_view(widgets, sender);
     }
 }
