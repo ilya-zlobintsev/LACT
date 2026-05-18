f54f395962cae1dd1ce954b9cd07e612cfa20b4b
Roman Makarov <rmakarov94@gmail.com>
2026-05-18 22:43:45 +0200

    fix: prevent SettingsChanged event during fan_curve_frame init

--- lact-gui/src/app/pages/thermals_page/fan_curve_frame.rs
+++ lact-gui/src/app/pages/thermals_page/fan_curve_frame.rs
@@ -327,7 +327,7 @@ impl relm4::Component for FanCurveFrame {
         let temp_keys = gtk::StringList::default();
         let current_temp_key = U32Binding::new(0u32);
 
-        let change_signals = [
+        let mut change_signals = [
             &spindown_delay_adj,
             &change_threshold_adj,
             &auto_threshold_adj,
@@ -339,15 +339,9 @@ impl relm4::Component for FanCurveFrame {
             });
             (adj.clone().upcast(), signal)
         })
-        .chain([(
-            current_temp_key.clone().upcast(),
-            current_temp_key.connect_value_notify(|_| {
-                APP_BROKER.send(AppMsg::SettingsChanged);
-            }),
-        )])
-        .collect();
+        .collect::<Vec<_>>();
 
-        let model = Self {
+        let mut model = Self {
             pmfw_options,
             is_dragging: Rc::new(AtomicBool::new(false)),
             speed_range: Rc::new(RefCell::new(DEFAULT_SPEED_RANGE)),
@@ -357,7 +351,7 @@ impl relm4::Component for FanCurveFrame {
             auto_threshold_adj,
             temp_keys,
             current_temp_key,
-            change_signals,
+            change_signals: Rc::default(),
             data: Rc::default(),
             drag_coord: Rc::default(),
             drag_point: Rc::default(),
@@ -368,6 +362,14 @@ impl relm4::Component for FanCurveFrame {
         let label_size_group = gtk::SizeGroup::new(gtk::SizeGroupMode::Horizontal);
         let spin_size_group = gtk::SizeGroup::new(gtk::SizeGroupMode::Horizontal);
         let widgets = view_output!();
+
+        change_signals.push((
+            model.current_temp_key.clone().upcast(),
+            model.current_temp_key.connect_value_notify(|_| {
+                APP_BROKER.send(AppMsg::SettingsChanged);
+            }),
+        ));
+        model.change_signals = change_signals.into();
 
         ComponentParts { model, widgets }
     }
