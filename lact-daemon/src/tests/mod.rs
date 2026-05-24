8d23a31fffc528b6b3e228d1e00a9e4df8393a01
Ilya Zlobintsev <ilya.zl@protonmail.com>
2026-05-24 19:27:26 +0300

    chore: move gpu test data to a shared directory (#1037)

--- lact-daemon/src/tests/mod.rs
+++ lact-daemon/src/tests/mod.rs
@@ -25,7 +25,7 @@ fn init_tracing() {
 async fn snapshot_everything() {
     init_tracing();
 
-    let test_data_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("src/tests/data");
+    let test_data_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("../tests/snapshots");
     let pci_db = read_pci_db();
 
     for vendor_dir in fs::read_dir(test_data_dir).unwrap().flatten() {
@@ -62,7 +62,7 @@ async fn apply_settings() {
 
     let local_set = LocalSet::new();
     local_set.spawn_local(async move {
-        let test_data_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("src/tests/data");
+        let test_data_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("../tests/snapshots");
         let pci_db = read_pci_db();
 
         for vendor_dir in fs::read_dir(test_data_dir).unwrap().flatten() {
