7c805f6ebf47ca8ea7b9e2ee98da7a0cf0dab6c5
Roman Makarov <rmakarov94@gmail.com>
2026-06-25 21:28:49 +0200

    handle case where linkBW and dp2LinkBW are 0

--- lact-daemon/src/server/gpu_controller/nvidia.rs
+++ lact-daemon/src/server/gpu_controller/nvidia.rs
@@ -1343,13 +1343,17 @@ impl GpuController for NvidiaGpuController {
                             match handle.get_dp_link_config(display_id) {
                                 Ok(params) => {
                                     *lanes = Some(params.laneCount.try_into()?);
-                                    *bandwidth = Some(if params.linkBW != 0 {
-                                        crate::server::display::dp1_rate_to_bandwidth(params.linkBW)
-                                    } else {
-                                        crate::server::display::dp2_rate_to_bandwidth(
+                                    *bandwidth = if params.linkBW != 0 {
+                                        Some(crate::server::display::dp1_rate_to_bandwidth(
+                                            params.linkBW,
+                                        ))
+                                    } else if params.dp2LinkBW != 0 {
+                                        Some(crate::server::display::dp2_rate_to_bandwidth(
                                             params.dp2LinkBW,
-                                        )
-                                    });
+                                        ))
+                                    } else {
+                                        None
+                                    };
                                 }
                                 Err(err) => {
                                     warn!("could not fetch DP info for display {key}: {err:#}");
