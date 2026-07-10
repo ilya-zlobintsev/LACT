924e978dc9a05733d27be811d825064a7178421e
Roman Makarov <rmakarov94@gmail.com>
2026-07-11 01:01:16 +0200

    fix

--- lact-schema/build.rs
+++ lact-schema/build.rs
@@ -2,6 +2,7 @@ use vergen_gitcl::{Emitter, Gitcl};
 
 fn main() {
     Emitter::default()
+        .default_on_error()
         .add_instructions(&Gitcl::builder().sha(true).build())
         .unwrap()
         .emit()
