5a16ece6a6f2141d71c6f59a62088331c50bc74b
WizardlyBump17 <davicraft130@gmail.com>
2026-07-19 16:39:43 -0300

    setting the remaining points based on the last point for Intel

--- lact-daemon/src/server/gpu_controller/intel.rs
+++ lact-daemon/src/server/gpu_controller/intel.rs
@@ -671,6 +671,23 @@ impl IntelGpuController {
             }
         }
 
+        let last_point: Option<(&i32, &f32)> = curve.0.iter().last();
+        let points_amount: usize = self.get_hwmon_controllable_points_amount() as usize;
+        if (last_point.is_some() && curve.0.len() < points_amount) {
+            for point in curve.0.len()..=points_amount {
+                let point_data: (&i32, &f32) = last_point.unwrap();
+                let pwm = (*point_data.1 * 255.0) as u8;
+                let temp = (*point_data.0 as f64 * 1000.0) as i32;
+
+                for fan_index in 1..=fans {
+                    self.write_hwmon_file(&[&format!("pwm{}_auto_point{}_temp", fan_index, point)], &temp.to_string())
+                        .context("Could not set the temperature for a point in the curve")?;
+                    self.write_hwmon_file(&[&format!("pwm{}_auto_point{}_pwm", fan_index, point)], &pwm.to_string())
+                        .context("Could not set the RPM for a point in the curve")?;
+                }
+            }
+        }
+
         return Result::Ok(true);
     }
 
